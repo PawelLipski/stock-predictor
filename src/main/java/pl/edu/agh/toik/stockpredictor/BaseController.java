2d8ab4598692b82b4641c6be02728f3de8367224
Paweł Lipski <pawel.p.lipski@gmail.com>
2015-05-15 16:43:36 +0200

    Sort out Java packages

--- src/main/java/pl/edu/agh/toik/stockpredictor/BaseController.java
+++ src/main/java/pl/edu/agh/toik/stockpredictor/BaseController.java
@@ -1,4 +1,4 @@
-package com.springapp.controller;
+package pl.edu.agh.toik.stockpredictor;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
