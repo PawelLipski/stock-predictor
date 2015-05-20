8b7f5eb21e2e7e6904154629d95e79723630fe43
Paweł Lipski <pawel.p.lipski@gmail.com>
2015-05-20 13:03:27 +0200

    Pass a java.util.Date as a RESTful method param

--- src/main/java/pl/edu/agh/toik/stockpredictor/RestController.java
+++ src/main/java/pl/edu/agh/toik/stockpredictor/RestController.java
@@ -1,14 +1,30 @@
 package pl.edu.agh.toik.stockpredictor;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RestController {	
+
+	// http://localhost:8080/StockPredictor/foo?date=2015-05-15
 	@RequestMapping(value = "/foo")
 	@ResponseBody
-	public int foo() { 
-		return 42;
+	public String foo(@RequestParam(value="date") Date s) { 
+		return s.toString();
 	}
+	
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+    }
 }
