13f9b681b9d86c93bfc3d43bdcc32db8828a75fa
Paweł Lipski <pawel.p.lipski@gmail.com>
2015-05-21 09:10:33 +0200

    Make CandlestickChart the return type of the REST method

--- src/main/java/pl/edu/agh/toik/stockpredictor/RestController.java
+++ src/main/java/pl/edu/agh/toik/stockpredictor/RestController.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.toik.stockpredictor;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -11,14 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import pl.edu.agh.toik.stockpredictor.technicalanalysis.chart.CandlestickChart;
+import pl.edu.agh.toik.stockpredictor.technicalanalysis.domain.Candle;
+import pl.edu.agh.toik.stockpredictor.technicalanalysis.domain.Formation;
+import pl.edu.agh.toik.stockpredictor.technicalanalysis.domain.ListedCompany;
+
 @Controller
 public class RestController {	
 
 	// http://localhost:8080/StockPredictor/foo?date=2015-05-15
 	@RequestMapping(value = "/foo")
 	@ResponseBody
-	public String foo(@RequestParam(value="date") Date s) { 
-		return s.toString();
+	public CandlestickChart foo(@RequestParam(value="date") Date s) {
+		CandlestickChart chart = new CandlestickChart();
+		ListedCompany listedCompany = new ListedCompany("Example", "EXA");
+		chart.setListedCompany(listedCompany);
+		chart.setStartDay(new Date(2015, 4, 10));
+		chart.setEndDay(new Date(2015, 4, 15));
+		chart.setCandles(new ArrayList<Candle>());
+		chart.setFormations(new ArrayList<Formation>());
+		return chart;
 	}
 	
     @InitBinder
